2ff24751dca02fb6d6b820a0af3694c2d5113e2a
Olof Karlsson <ollekarlsson.88@gmail.com>
2018-11-08 14:56:26 +0100

    updated test, try to interpolate env vars

--- testrunner/__tests__/test.js
+++ testrunner/__tests__/test.js
@@ -2,6 +2,9 @@ const fetch = require('node-fetch')
 const SERVICE_B_URI = process.env.SERVICE_B_URI
 
 test('request body is echoed via serviceb', async () => {
-  const msg = await fetch(`${SERVICE_B_URI}/relay`, {method: 'POST', body: 'foobar'}).then(resp => resp.text())
-  expect(msg).toBe('foobar') // Test is failing!
+  const resp = await fetch(`${SERVICE_B_URI}/relay`, {method: 'POST', body: 'foobar'})
+  const text = await resp.text()
+
+  expect(resp.statusCode).toBe(200)
+  expect(text).toBe('foobar')
 })
